152f9f4d7b7c558cee02dfb7b9f32febe26a23f3
hammadmirzaa <hamadbaig25@gmail.com>
2026-01-21 12:35:48 +0500

    Added separate docker and app.yaml

--- todo-backend/src/main.ts
+++ todo-backend/src/main.ts
@@ -1,24 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import next from 'next';
-import { Request, Response } from 'express';
 
 async function bootstrap() {
-  const dev = process.env.NODE_ENV !== 'production';
-  const appNext = next({ dev });
-  const handle = appNext.getRequestHandler();
-
-  await appNext.prepare();
-
   const app = await NestFactory.create(AppModule);
-
-  // Serve Next.js pages through Nest
-  app.use((req: Request, res: Response, next) => {
-    if (req.url.startsWith('/api')) return next();
-    return handle(req, res);
-  });
-
-  const port = process.env.PORT || 4000;
-  await app.listen(port);
+    app.enableCors();
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
